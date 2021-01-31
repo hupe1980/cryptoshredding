6ef458256302a52f27500aa314b34519cbdb04aa
hupe1980 <frankhuebner1980@gmail.com>
2021-01-31 22:37:03 +0100

    Misc

---
+++ README.rst
@@ -0,0 +1,2 @@
+# cryptoshredding
+Crypto shredding is the practice of 'deleting' data through the destruction of the cryptographic keys protecting the data. 
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+import re
+
+from setuptools import find_packages, setup
+
+ROOT = os.path.dirname(__file__)
+VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+
+requires = [
+    'boto3>=1.16.63',
+    'dynamodb-encryption-sdk>=1.2.0',
+]
+
+
+def get_version():
+    init = open(os.path.join(ROOT, 'cryptoshredding', '__init__.py')).read()
+    return VERSION_RE.search(init).group(1)
+
+
+setup(
+    name='cryptoshredding',
+    version=get_version(),
+    description='Crypto shredding for Python',
+    long_description=open('README.rst').read(),
+    author='hupe1980',
+    url='https://github.com/hupe1980/cryptoshredding',
+    scripts=[],
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    install_requires=requires,
+    license="MIT",
+    python_requires=">= 3.6",
+    classifiers=[
+        'Development Status:: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
